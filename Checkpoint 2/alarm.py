f04f892b4bfdc23f623a3af5a6da66412e34c324
Aileen <aileenmagadanruiz@gmail.com>
2025-08-21 17:29:38 +0000

    Alarm

---
+++ Checkpoint 2/alarm.py
@@ -0,0 +1,18 @@
+def main():
+    is_armed = False
+    motion_detectant = True
+    door_open = False
+    is_afternoon = True
+    display_alarm(is_armed, motion_detectant, door_open, is_afternoon)
+    
+def display_alarm(iar, ms, dop, an):
+    if iar:
+        if ms:
+            print("INTRUDER!!!!!!")
+        if dop:
+            print("door is open")
+    else:
+        if an and ms:
+            print("Welcome home! Turning the light on!")
+            
+main()
