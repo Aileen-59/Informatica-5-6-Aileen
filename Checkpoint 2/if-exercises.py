fb2dfab007f0a9f54dc4e2c0fd8f3c8acfb7d3cf
Aileen <aileenmagadanruiz@gmail.com>
2025-08-21 19:55:22 +0000

    if exercises

---
+++ Checkpoint 2/if-exercises.py
@@ -0,0 +1,9 @@
+def main():
+    number = int(input("Write an integer number"))
+    if number < 0:
+        print(number * -1)
+    if number > 0:
+        print (number)
+        
+        
+main()   
