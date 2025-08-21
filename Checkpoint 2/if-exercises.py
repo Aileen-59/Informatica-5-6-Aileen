ad8304ae910f7280b6a76c92a4ff9dc9d6e299d5
Aileen <aileenmagadanruiz@gmail.com>
2025-08-21 20:20:06 +0000

    if exercises

--- Checkpoint 2/if-exercises.py
+++ Checkpoint 2/if-exercises.py
@@ -1,5 +1,5 @@
 def main():
-    number = int(input("Write an integer number"))
+    number = int(input("Write an integer number: "))
     if number < 0:
         print(number * -1)
     if number > 0:
@@ -7,3 +7,19 @@ def main():
         
         
 main()   
+
+#Exercise 2
+def main():
+    number1 = int(input("First number: ")) 
+    number2 = int(input("Second number:"))
+    op = int(input("Write an operation: "))
+    addition = number1 + number2
+    multiplication = number1 * number2
+    substraction = number1 - number2
+    operation(number1, number2, op,addition, multiplication, substraction)
+    
+    
+def operation(op, add, multiplication, substract):
+    if op == addition:
+        print(int addition)
+        
