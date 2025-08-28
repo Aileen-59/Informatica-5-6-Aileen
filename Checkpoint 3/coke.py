810ecaef62892b88d6e61666cadebcc35dcd4602
Aileen <aileenmagadanruiz@gmail.com>
2025-08-28 20:08:02 +0000

    coke

--- Checkpoint 3/coke.py
+++ Checkpoint 3/coke.py
@@ -1,12 +1,22 @@
 def main():
-    
-name = input("Write your name: ")
-coke = 50 
-coin = 0
+    price = 50
+    total_paid = 0
+    vending_machine(price, total_paid)
 
-while coin > coke:
-    print("Amount due:"(coke - coin))
-    
 
+def vending_machine(price, total_paid):
+    while price > 0:
+        print(F"Amount due: {price}")
+        pay = int(input("Insert a coin: "))
+        
+        if pay == 25 or pay == 10 or pay == 5:
+            price = price - pay
+            total_paid = total_paid + pay
+        else:
+            print("NOT a coin!")
+        
+    if total_paid >= 50:
+        print("THANKS! Here's your coke")
+        print(f"Here's your change: {total_paid - 50}")
 
 main()
