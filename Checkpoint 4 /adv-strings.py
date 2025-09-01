134af15697466eee2c88645d6cb289a483ea1615
Aileen <aileenmagadanruiz@gmail.com>
2025-09-01 19:40:04 +0000

    adv-strings

---
+++ Checkpoint 4 /adv-strings.py
@@ -0,0 +1,28 @@
+#String length
+input_string = input("Please type a message: ")
+# print(input_string)
+
+# print("-" *len(input_string))
+
+# #String Index
+# print(input_string[0])
+# print(input_string[1])
+# print(input_string[2])
+# print(input_string[3])
+# print(input_string[4])
+
+#Example
+# i = 0
+# while i < len(input_string):
+#     print(input_string[i])
+#     i += 1
+
+# i = -1
+# while i < len(input_string):
+#     print(input_string[i])
+#     i += -1
+
+print("h" in input_string)
+print("j" in input_string)
+print("ello" in input_string)
+print(input_string.find ("h"))
