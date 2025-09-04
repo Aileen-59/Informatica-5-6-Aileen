551789d96b09ef020d8643c2519a2bc87a0b50e2
Aileen <aileenmagadanruiz@gmail.com>
2025-09-04 17:28:25 +0000

    multiplication

---
+++ Checkpoint 4 /multiplication.py
@@ -0,0 +1,7 @@
+def main():
+    number = int(input("Enter a positive number: "))
+    multiplication(number)
+    
+def multiplication(number):
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    table = number * numbers
