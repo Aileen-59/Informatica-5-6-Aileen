469b1545ba7181bd8a10d8241f80e706ab4334bb
Aileen <aileenmagadanruiz@gmail.com>
2025-09-04 16:24:07 +0000

    cipher

--- Checkpoint 4 /cipher.py
+++ Checkpoint 4 /cipher.py
@@ -2,7 +2,7 @@ def main():
     user_message = input("Type a message: ").lower()
     encode_message(user_message)
     
-    def encode_message (text):
+def encode_message (text):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     cipher = "zyxwvutsrqponmlkjihgfedcba"
     new_message = ""
@@ -10,4 +10,13 @@ def main():
     
     while i < len(text):
         char = text[i]
+        
+        if char in alphabet:
+            cipher_index = alphabet.find(char)
+            new_message += cipher[cipher_index]
+        else:
+            new_message += char
+        i += 1
+    print(new_message)
+    
 main()
