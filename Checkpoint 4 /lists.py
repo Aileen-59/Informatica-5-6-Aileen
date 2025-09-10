5276c19ed81937776093d1db844b94db0377e4bb
Aileen <aileenmagadanruiz@gmail.com>
2025-09-10 20:19:47 +0000

    radio playlist

--- Checkpoint 4 /lists.py
+++ Checkpoint 4 /lists.py
@@ -6,12 +6,12 @@ print(len(independence_stages))                     #print the length
 leaders = []
 leaders.append("Miguel Hidalgo")
 leaders.append("Vicente Guerrero")
-# leaders.extend(independence_stages) //Mix lists together
+# leaders.extend(independence_stages)                   //Mix lists together
 leaders.insert(1, "José María Morelos")
-# leaders.remove("Vicente Guerrero")//Remove an item
+# leaders.remove("Vicente Guerrero")                    //Remove an item
 leaders.append(input("Type a leader: "))
-# leaders.pop(1)//It is the same as "remove" but it removes by index or positions
-#leaders.clear()//Clear the list
+# leaders.pop(1)                                    //It is the same as "remove" but it removes by index or positions
+#leaders.clear()                                        //Clear the list
 print(leaders.index("Miguel Hidalgo"))              #To find the position of an item 
 print(leaders.count("Vicente Guerrero"))            #To count how many times an item appears in the list
 # leaders.sort()                                       #Sort the list alphabetically
---
+++ Checkpoint 5/radio-playlist.py
@@ -0,0 +1,16 @@
+weekly_playlist = ["Blinding Lights", " Levitating", "As It Was", "Heat Waves", "Good 4 u"]
+
+weekly_playlist= []
+weekly_playlist.append("Drivers License")
+weekly_playlist.insert(0, "Bohemian Rhapsody")
+weekly_playlist.remove("Good 4 u")
+print(weekly_playlist.index("Levitating"))
+print(weekly_playlist.count("As It Was"))
+
+throwback_thursday = weekly_playlist.copy()
+throwback_thursday.reverse()
+
+throwback_thursday = weekly_playlist.copy()
+throwback_thursday.sort()
+
+print(weekly_playlist)
