c78cb09dc0214a31538bb51db53bed8bbd6c7f2b
Aileen <aileenmagadanruiz@gmail.com>
2025-09-12 17:06:10 +0000

    radio playlist

--- Checkpoint 5/radio-playlist.py
+++ Checkpoint 5/radio-playlist.py
@@ -1,14 +1,23 @@
-weekly_playlist = ["Blinding Lights", " Levitating", "As It Was", "Heat Waves", "Good 4 u"]
+weekly_playlist = ["Blinding Lights", "Levitating", "As It Was", "Heat Waves", "Good 4 u"]
+print("This is your weekly playlist:", weekly_playlist)
 
-
+print("This songs will be added to your playlist: ")
 weekly_playlist.append("Drivers License")
+
+print("I will add this song to the beginning of your playlist:")
 weekly_playlist.insert(0, "Bohemian Rhapsody")
+print(weekly_playlist)
+
+print("I will remove this song from your list: ")
 weekly_playlist.remove("Good 4 u")
 weekly_playlist.index("Levitating")
 weekly_playlist.count("As It Was")
+print(weekly_playlist)
+
 
 throwback_thursday = weekly_playlist.copy()
 throwback_thursday.reverse()
+print(throwback_thursday)
 
 throwback_thursday = weekly_playlist.copy()
 throwback_thursday.sort()
