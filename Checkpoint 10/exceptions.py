a2658536a99702bf71ac61ee6f7412b47a7fa34d
Aileen <aileenmagadanruiz@gmail.com>
2025-10-23 19:58:30 +0000

    exceptions.py

--- Checkpoint 10/exceptions.py
+++ Checkpoint 10/exceptions.py
@@ -24,12 +24,27 @@
 # print(spam(1))
 
 
-while True:
-    try:
-        x = int(input("What's x? "))
-    except ValueError:
-        print("x is not a number")
-    else:
-        break
+# while True:
+#     try:
+#         x = int(input("What's x? "))
+#     except ValueError:
+#         print("x is not a number")
+#     else:
+#         break
 
-print(f"x is equal to {x}")
+# print(f"x is equal to {x}")
+
+
+def read_small_integer():
+    while True:
+        try:
+            input_str = input("Please type a number: ")
+            number = int(input_str)
+            if number < 100 and number >= 0:
+                return number
+        except ValueError:
+            pass
+        print("This input is invalid. Please try again.")
+        
+number = read_small_integer()
+print(number, "to the power of three is", number ** 3)
