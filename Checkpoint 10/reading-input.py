7907167ecbae1026347923ad130b47d6646bfdde
Aileen <aileenmagadanruiz@gmail.com>
2025-10-28 16:38:34 +0000

    fuel

--- Checkpoint 10/reading-input.py
+++ Checkpoint 10/reading-input.py
@@ -3,15 +3,16 @@ def main():
   print("You typed in:", number)
 
 def read_input(prompt, small, large): # Insert missing parameters
-    try:
-        x = int(input(prompt))
-        if 5 < x < 10:
-            return x
-        else:
-            print("Please type a NUMBER between", small, "and", large )
-    except ValueError:
-        print("Please type a NUMBER between", small , "and", large )
-    
+    while True:
+        try:
+            x = int(input(prompt))
+            if 5 < x < 10:
+                return x
+            else:
+                print("Please type a NUMBER between", small, "and", large )
+        except ValueError:
+            print("Please type a NUMBER between", small , "and", large )
+            
 
 
 
