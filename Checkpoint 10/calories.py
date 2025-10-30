d31417a00e0796058148c74d9f34f7d947781b2d
Aileen <aileenmagadanruiz@gmail.com>
2025-10-30 20:20:11 +0000

    calories

---
+++ Checkpoint 10/calories.py
@@ -0,0 +1,27 @@
+food = {
+    "Whole milk": "73",
+    "Almond milk": "30",
+    "Greek yogurt fat free": "120",
+    "Sour cream, light": "16",
+    "Egg": "75",
+    "Egg white": "16",
+    "Blue cheese": "119",
+    "Cream cheese": "51",
+    "Almonds": "170",
+    "Peanuts": "166",
+    "Black beans": "113",
+    "Chickpeas": "105",
+    "Atlantic salmon": "175",
+    "Chicken Deli": "20",
+    "Broccoli": "7",
+    "Corn": "59",
+    "Watermelon": "11",
+    "Avocado": "48",
+    "Oatmeal": "77",
+    "Ranch": "73"
+    }
+    
+first = input("Enter your first selection: ")
+second = input("Enter your second selection: ")
+ 
+    
