a2a101d2418a160a785cfaf961d339b374e5a2fc
Aileen <aileenmagadanruiz@gmail.com>
2025-10-30 16:47:44 +0000

    fuel.py

--- Checkpoint 10/fuel.py
+++ Checkpoint 10/fuel.py
@@ -1,10 +1,28 @@
 def main():
-    fuel = input("Enter fuel level x/y:")
-    fuel.split("/")
+    fuel()
     
-    
-    print(fuel)
-    
-    
-    
+def fuel():
+    condition = True
+    while condition:
+        try:
+            fraction = input("Fuel fraction: ") .split("/")
+            num = int(fraction [0])
+            den = int(fraction [1])
+            percentage = round((num/den)*100)
+            if percentage > 100:
+                print("Invalid input. Fuel cannot be more than 100%.")
+            elif percentage >= 99:
+                print("F")
+                break
+            elif percentage <= 1:
+                print("E")
+                condition = False
+            else:
+                print(f"The percentage is {percentage}%")
+                condition = False
+        except ZeroDivisionError:
+            print("Invalid fraction.")
+        except ValueError:
+            print("Please enter numbers only.")
+            
 main()
