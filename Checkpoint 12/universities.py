256442b8ffa981f8fe308ce96c1987ced0a8e024
Aileen <aileenmagadanruiz@gmail.com>
2025-11-10 19:36:53 +0000

    universities

---
+++ Checkpoint 12/universities.py
@@ -0,0 +1,6 @@
+universities = {
+    "Eastern Arizona College" :{"careers": "90",
+                                "cost": "4776",
+                                "distance": "404.4 km"}
+    "UACJ" :{"careers":}             
+}
