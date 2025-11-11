249fd38cf2c81a6508424c2ccca551d6fc9c8f26
Aileen <aileenmagadanruiz@gmail.com>
2025-11-11 16:39:29 +0000

    universities

--- Checkpoint 12/universities.py
+++ Checkpoint 12/universities.py
@@ -1,6 +1,10 @@
 universities = {
-    "Eastern Arizona College" :{"careers": "90",
-                                "cost": "4776",
-                                "distance": "404.4 km"}
-    "UACJ" :{"careers":}             
+    "Eastern Arizona College":{"distance":"404.4","num_majors": "90","price":"8370"},
+    "UACJ":{"distance":"11.3","num_majors":"60","price":"2250"},
+    "Tecmilenio":{"distance":"202.1","num_majors":"43","price":"30000"},
+    "LaSalle":{"distance":"299.7","num_majors":"42","price":"60000"},
+    "UTP":{"distance":"8.8","num_majors":"4","price":"2505"}
 }
+def main():
+    name = input("Type the name of the university: ")
+
